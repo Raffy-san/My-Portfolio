302c2332dfb197c98623bd514d15993407a6d953
Rafael Sanoria <rafaelsanoria506@gmail.com>
2025-11-01 12:54:19 +0800

    On branch Raffy  Your branch is up to date with 'origin/Raffy'.  Changes to be committed: 	modified:   public/index.html 	modified:   public/projects.html 	modified:   src/components/header.html 	modified:   src/css/input.css 	modified:   src/css/output.css 	modified:   src/js/include.js

--- src/js/include.js
+++ src/js/include.js
@@ -6,6 +6,24 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.text())
             .then(data => {
                 element.innerHTML = data;
+
+                const menuBtn = document.getElementById('menuBtn');
+                const mobileMenu = document.getElementById('mobileMenu');
+                const overlay = document.getElementById('overlay');
+
+                menuBtn.addEventListener("click", () => {
+                    mobileMenu.classList.toggle('-translate-x-full');
+                    mobileMenu.classList.toggle('-translate-x-0');
+
+                    overlay.classList.toggle('hidden');
+                });
+
+                overlay.addEventListener("click", () => {
+                    mobileMenu.classList.toggle('-translate-x-full');
+                    mobileMenu.classList.toggle('-translate-x-0');
+
+                    overlay.classList.toggle('hidden');
+                })
             });
     });
 });
